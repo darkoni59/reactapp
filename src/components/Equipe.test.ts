3f193cb0eb952cb1ca2d3572bd1ffa1692757efd
bryanbultot <darkoni59610@gmail.com>
2020-06-24 10:17:54 +0200

    test unitaire

---
+++ src/components/Equipe.test.ts
@@ -0,0 +1,6 @@
+import Equipe from './Equipe';
+import ListItemText from './Equipe';
+
+it ('test',()=>{
+    expect(ListItemText).toEqual(Equipe);
+})
--- src/components/Gestion.test.ts
+++ src/components/Gestion.test.ts
@@ -1,6 +1,14 @@
 import combat from './Gestion';
 import onClick from './Gestion';
+import addHero from './Gestion';
+import choixNomPartie from './Gestion';
 
 it('test', () => {
     expect(onClick).toEqual(combat);
+});
+it('test2',()=>{
+    expect(onClick).toEqual(addHero);
+});
+it ('test3',()=>{
+expect(onClick).toEqual(choixNomPartie);
 });
